9e1e0b415a01ea49f0ec517ce33d9f4f015f5450
wert_and-ПК\wert_and <alexandr_works@ukr.net>
2017-06-30 11:21:02 +0300

    first finish version

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
 
+    /// Scrolling page to anchor
+
+    var navigationMenu = $(".bl_mainNavigation");
+
+    navigationMenu.on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 400);
+    });
 
 
     // SLIDER

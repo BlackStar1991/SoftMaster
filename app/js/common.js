000f03585683c7a3815927397ca979000c677ddd
wert_and-ПК\wert_and <alexandr_works@ukr.net>
2017-06-30 16:17:59 +0300

    first step of optimisation

--- app/js/common.js
+++ app/js/common.js
@@ -23,15 +23,26 @@ $(document).ready(function () {
                 slidesToShow: 5,
                 slidesToScroll: 1,
                 centerPadding: '10px',
-                responsive: [
-                    {
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
+                responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        infinite: true
                     }
-                ]
+
+                }, {
+
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 3
+                    }
+
+                }, {
+
+                    breakpoint: 300,
+                    settings: "unslick" // destroys slick
+
+                }]
 
             }
         );

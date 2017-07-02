5532023cdc9396f1ffeaa95f7a4578b9134cbb7d
BlackStar1991 <zirkaandry@ukr.net>
2017-07-02 23:20:44 +0300

    adaptive site

--- app/js/common.js
+++ app/js/common.js
@@ -26,21 +26,16 @@ $(document).ready(function () {
                 responsive: [{
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 4,
-                        infinite: true
+                        slidesToShow: 4
                     }
 
                 }, {
 
-                    breakpoint: 600,
+                    breakpoint: 767,
                     settings: {
-                        slidesToShow: 3
+                        slidesToShow: 3,
+                        slidesToScroll: 2
                     }
-
-                }, {
-
-                    breakpoint: 300,
-                    settings: "unslick" // destroys slick
 
                 }]
 
